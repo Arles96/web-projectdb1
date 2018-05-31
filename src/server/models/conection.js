f16e920d2d2d856f071406e6c62d6e844e494b2e
Arles96 <arles.cerrato@unitec.edu>
2018-05-31 06:37:39 -0600

    Modificacion de la base de datos

--- src/server/models/conection.js
+++ src/server/models/conection.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql'
 
 const connection = mysql.createConnection({
-    host : 'mysql',
+    host : 'k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port : '3306',
-    user : 'database',
-    password : 'HelloWorld',
-    database : 'tabacalera123'
+    user : 'j303kx7r2zulke5c',
+    password : 'qyzcfrteuldo6d6r',
+    database : 'yfzmyuv46lj1dits'
 })
 
 connection.connect((err)=>{
